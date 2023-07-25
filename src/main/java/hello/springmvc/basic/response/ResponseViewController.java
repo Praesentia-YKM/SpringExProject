b774a9910df2c7b0b69693ea6c8e151649bb5b70
Praesentia-YKM <ykm8864@naver.com>
2023-07-25 20:26:32 +0900

    Void를 반환하는 경우 @Controller 를 사용하고, HttpServletResponse , OutputStream(Writer) 같은 HTTP 메시지 바디를 처리하는 파라미터가 없으면 요청 URL을 참고해서 논리 뷰 이름으로 사용

--- src/main/java/hello/springmvc/basic/response/ResponseViewController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -13,12 +13,15 @@ public class ResponseViewController {
                 .addObject("data", "hello!");
         return mav;
     }
+
     @RequestMapping("/response-view-v2")
     public String responseViewV2(Model model) {
 
         model.addAttribute("data", "hello!!");
         return "response/hello";
     }
+
+    // 컨트롤러의 url경로와 view의 논리적 뷰의 이름이 같으면 void로하고 반환형 없어도 됨.
     @RequestMapping("/response/hello")
     public void responseViewV3(Model model) {
 
